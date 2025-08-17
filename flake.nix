125b6f41b7df4af0d34356b2ac8769b360ced904
abbesm0hamed <abbesmohamed717@gmail.com>
2025-08-17 13:32:38 +0100

    fix: nixos version

--- flake.nix
+++ flake.nix
@@ -25,8 +25,6 @@
             home-manager.nixosModules.home-manager
             {
               home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
                 users.abbes = import ./home/workstation.nix;
               };
             }
