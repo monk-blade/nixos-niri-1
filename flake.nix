3dd36301d9769f44d82130f5c7f4bf824655beec
abbesm0hamed <abbesmohamed717@gmail.com>
2025-08-19 19:01:42 +0100

    fix: stylix flake url

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:nix-community/stylix/release-25.05";
   };
 
   outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs: {
