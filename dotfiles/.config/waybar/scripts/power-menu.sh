fd8f76b7eb81d8d470e620d3b4b858533de6efad
abbesm0hamed <abbesmohamed717@gmail.com>
2025-08-24 02:42:09 +0100

    refactor(waybar): update workspaces style

---
+++ dotfiles/.config/waybar/scripts/power-menu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Power menu options
+options="🔒 Lock\n🚪 Logout\n💤 Suspend\n🔄 Reboot\n⚡ Shutdown"
+
+# Show fuzzel menu and get selection
+selected=$(echo -e "$options" | fuzzel --dmenu --prompt="Power: " --width=30 --lines=5)
+
+case $selected in
+    "🔒 Lock")
+        hyprlock
+        ;;
+    "🚪 Logout")
+        niri msg action quit
+        ;;
+    "💤 Suspend")
+        systemctl suspend
+        ;;
+    "🔄 Reboot")
+        systemctl reboot
+        ;;
+    "⚡ Shutdown")
+        systemctl poweroff
+        ;;
+esac
