f622697771cd0ffcc283e4a82f229511f0b0e006
abbesm0hamed <abbesmohamed717@gmail.com>
2025-08-24 06:11:43 +0100

    fix: tmux issues

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -12,10 +12,7 @@
     # Install plugins using Nix
     plugins = with pkgs.tmuxPlugins; [
       # Essential plugins
-      {
-        plugin = vim-tmux-navigator;
-        extraConfig = "";
-      }
+      vim-tmux-navigator
       {
         plugin = yank;
         extraConfig = ''set -g @yank_selection 'primary'
@@ -31,64 +28,6 @@
         extraConfig = ''set -g @continuum-restore 'on'
         '';
       }
-      # Mode indicator plugin
-      {
-        plugin = pkgs.tmuxPlugins.mkTmuxPlugin {
-          pluginName = "tmux-mode-indicator";
-          version = "unstable-2023-06-26";
-          src = pkgs.fetchFromGitHub {
-            owner = "MunifTanjim";
-            repo = "tmux-mode-indicator";
-            rev = "20b0975df5741d3a7cff5b9b7d00330ade8a4086";
-            sha256 = "sha256-JlKKGNaKjbHaKcwWKyqKNq9P+nMZ0K3QgdDNpCeaiQg=";
-          };
-        };
-        extraConfig = ''
-          # Mode indicator settings with Kanagawa colors
-          set -g @mode_indicator_empty_prompt ' N '
-          set -g @mode_indicator_empty_mode_style 'bg=#7E9CD8,fg=#1F1F28'
-          set -g @mode_indicator_prefix_prompt ' P '
-          set -g @mode_indicator_prefix_mode_style 'bg=#D27E99,fg=#1F1F28'
-          set -g @mode_indicator_copy_prompt ' C '
-          set -g @mode_indicator_copy_mode_style 'bg=#a8e6cf,fg=#1F1F28'
-          set -g @mode_indicator_sync_prompt ' S '
-          set -g @mode_indicator_sync_mode_style 'bg=#DCA561,fg=#1F1F28'
-        '';
-      }
-      # Git status plugin
-      {
-        plugin = pkgs.tmuxPlugins.mkTmuxPlugin {
-          pluginName = "tmux-simple-git-status";
-          version = "unstable-2023-01-14";
-          src = pkgs.fetchFromGitHub {
-            owner = "kristijanhusak";
-            repo = "tmux-simple-git-status";
-            rev = "287da42f47d7204618b62f2c4f8bd60b36d5c7ed";
-            sha256 = "sha256-2eiQNPOaDV2CBXcS4dAOUzdl3bPcNlp6Jz0lgMNgOHs=";
-          };
-        };
-        extraConfig = ''
-          # Git status colors using Kanagawa palette
-          set -g @simple_git_status_branch_color "#7E9CD8"
-          set -g @simple_git_status_added_color "#76946A"
-          set -g @simple_git_status_deleted_color "#C34043"
-          set -g @simple_git_status_modified_color "#DCA561"
-          set -g @simple_git_status_renamed_color "#957FB8"
-          set -g @simple_git_status_untracked_color "#727169"
-          
-          # Git status symbols (using nerd fonts)
-          set -g @simple_git_status_branch_symbol "󰘬 "
-          set -g @simple_git_status_staged_symbol "󰱒 "
-          set -g @simple_git_status_changes_symbol "󰏫 "
-          set -g @simple_git_status_untracked_symbol "󰝦 "
-          set -g @simple_git_status_stashed_symbol "󱉟ﰗ "
-          set -g @simple_git_status_ahead_symbol " "
-          set -g @simple_git_status_behind_symbol " "
-          set -g @simple_git_status_conflict_symbol "󰕚 "
-          set -g @simple_git_status_clean_symbol "󰗠 "
-        '';
-      }
-      # Open plugin
       open
     ];
     
