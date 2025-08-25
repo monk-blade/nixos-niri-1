091e7be2afafa549df5da0c8577781bf8ae4befe
abbesm0hamed <abbesmohamed717@gmail.com>
2025-08-25 14:14:24 +0100

    fix: power menu icons and fuzzel border colors

--- dotfiles/.config/waybar/scripts/power-menu.sh
+++ dotfiles/.config/waybar/scripts/power-menu.sh
@@ -7,7 +7,7 @@ options=" Lock\n󰗽 Logout\n󰤄 Suspend\n󰜉 Reboot\n󰐥 Shutdown"
 selected=$(echo -e "$options" | fuzzel --dmenu --prompt="󰐥 Power: " --width=32 --lines=5)
 
 case $selected in
-    " Lock")
+    "󰌾 Lock")
         hyprlock
         ;;
     "󰗽 Logout")
