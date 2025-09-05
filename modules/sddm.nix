0b55404e339fee71a54320576039ef524b177bea
abbesm0hamed <abbesmohamed717@gmail.com>
2025-09-05 17:12:20 +0100

    fix: sddm wallpaper setup

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -1,55 +1,19 @@
 { config, lib, pkgs, ... }:
 
-let
-  locals = import ../hosts/workstation/locals.nix { inherit pkgs; };
-in
 {
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     
-    # Theme configuration
-    theme = "breeze";
-    
-    # Custom settings
     settings = {
       General = {
-        # Use custom background image
-        Background = toString locals.wallpapers.blurred;
-        
-        # Disable user avatars for cleaner look
-        EnableAvatars = false;
-        
-        # Session configuration
+        # Set your custom wallpaper path
+        Background = "/home/abbes/.config/backgrounds/blurry-snaky.jpg";
         DefaultSession = "niri.desktop";
-      };
-      
-      Theme = {
-        # Font configuration
-        Font = "SpaceMono Nerd Font";
-        FontSize = "12";
-        
-        # Colors (Catppuccin Mocha theme)
-        Current = "breeze";
-        CursorTheme = "Adwaita";
-        CursorSize = "24";
-      };
-      
-      Users = {
-        # User configuration
-        MaximumUid = "60000";
-        MinimumUid = "1000";
-        
-        # Hide shell users
-        HideShells = "/bin/false,/usr/bin/nologin,/sbin/nologin";
       };
     };
   };
-  
-  # Required packages for SDDM theming
-  environment.systemPackages = with pkgs; [
-    libsForQt5.qt5.qtgraphicaleffects
-    libsForQt5.qt5.qtquickcontrols2
-    libsForQt5.qt5.qtsvg
-  ];
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = false;
 }
